f0b0ea80304750f65b329aba63b243fe74ce1af1
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-04-30 09:48:14 +0200

    Inital commit

---
+++ src/main/java/org/example/alphamanagement/AlphaManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.alphamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/alphamanagement/AlphaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.alphamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
