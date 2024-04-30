df25780670b8fa42598ec9e77729a5d7302a24db
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-04-30 11:05:26 +0200

    Added basic attributes to Repository

--- src/main/java/org/example/alphamanagement/repository/AlphaRepository.java
+++ src/main/java/org/example/alphamanagement/repository/AlphaRepository.java
@@ -1,7 +1,14 @@
 package org.example.alphamanagement.repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AlphaRepository {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String user;
+    @Value("${spring.datasource.password}")
+    private String password;
 }
