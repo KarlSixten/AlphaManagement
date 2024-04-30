02acbefa64cf21a5d664fa7df1deb80172a14d75
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-04-30 11:05:16 +0200

    Added basic attributes and constructor to Service

--- src/main/java/org/example/alphamanagement/service/AlphaService.java
+++ src/main/java/org/example/alphamanagement/service/AlphaService.java
@@ -1,7 +1,13 @@
 package org.example.alphamanagement.service;
 
+import org.example.alphamanagement.repository.AlphaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AlphaService {
+    private final AlphaRepository alphaRepository;
+
+    public AlphaService(AlphaRepository alphaRepository) {
+        this.alphaRepository = alphaRepository;
+    }
 }
