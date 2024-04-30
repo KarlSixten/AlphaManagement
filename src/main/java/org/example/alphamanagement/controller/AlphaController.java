442312eca49370ec2554892b86b0e6a33c25e1ab
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-04-30 11:02:56 +0200

    Added basic attributes and constructor to Controller

--- src/main/java/org/example/alphamanagement/controller/AlphaController.java
+++ src/main/java/org/example/alphamanagement/controller/AlphaController.java
@@ -1,7 +1,18 @@
 package org.example.alphamanagement.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.example.alphamanagement.service.AlphaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("")
 public class AlphaController {
+    private final AlphaService alphaService;
+    private final HttpSession httpSession;
+
+    public AlphaController(AlphaService wishyService, HttpSession httpSession) {
+        this.alphaService = wishyService;
+        this.httpSession = httpSession;
+    }
 }
