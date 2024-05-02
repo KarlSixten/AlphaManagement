6c1c31a7cd672976db6fbd2d328201c75a926d1d
AndersBondeLudvigsen <anlu0001@stud.kea.dk>
2024-05-02 10:25:34 +0200

    Create emp And check uniqe emp

--- src/main/java/org/example/alphamanagement/repository/AlphaRepository.java
+++ src/main/java/org/example/alphamanagement/repository/AlphaRepository.java
@@ -1,7 +1,15 @@
 package org.example.alphamanagement.repository;
 
+import org.example.alphamanagement.model.Emp;
+import org.example.alphamanagement.repository.util.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
 
 @Repository
 public class AlphaRepository {
@@ -11,4 +19,58 @@ public class AlphaRepository {
     private String user;
     @Value("${spring.datasource.password}")
     private String password;
+
+
+    public Emp createUser(Emp newEmp) {
+        if (checkUniqueUsername(newEmp)) {
+            String sql = "INSERT INTO emp(username, password,jobType) VALUES (?, ?,?);";
+            Connection connection = ConnectionManager.getConnection(url, user, password);
+
+            try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setString(1, newEmp.getUsername());
+                pstmt.setString(2, newEmp.getPassword());
+                pstmt.executeUpdate();
+                return newEmp;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return null;
+        }
+    }
+
+
+    public boolean checkUniqueUsername(Emp empToCheck) {
+        boolean nameIsUnique = false;
+
+        String sql = "SELECT COUNT(*) FROM emp WHERE username like (?);";
+        Connection connection = ConnectionManager.getConnection(url, user, password);
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, empToCheck.getUsername());
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                int count = rs.getInt("COUNT(*)");
+                nameIsUnique = (count == 0);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return nameIsUnique;
+    }
+
+    public String createUserID(String fullName) {
+        String userID;
+        String[] names = fullName.split("\\s+");
+        String userIDLetters = names[0].substring(0, 2).toLowerCase() +
+                names[names.length - 1].substring(0, 2).toLowerCase();
+        Random random = new Random();
+        int numbers = random.nextInt(10000);
+        userID = String.format("%s%04d", userIDLetters, numbers);
+
+        return userID;
+    }
+
 }
+
+
