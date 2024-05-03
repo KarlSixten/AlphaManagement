bea7cb9c7f1a14eefe5eb29d2691204b104c7595
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-05-03 10:30:29 +0200

    Added first and last name as attribute to Emp class

--- src/main/java/org/example/alphamanagement/model/Emp.java
+++ src/main/java/org/example/alphamanagement/model/Emp.java
@@ -1,11 +1,15 @@
 package org.example.alphamanagement.model;
 
 public class Emp {
+    private String firstName;
+    private String lastName;
     private String username;
     private String password;
     private int jobType;
 
-    public Emp(String username, String password, int jobType) {
+    public Emp(String firstName, String lastName, String username, String password, int jobType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.username = username;
         this.password = password;
         this.jobType = jobType;
